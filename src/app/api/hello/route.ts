ac887649a76bb32a34941818418f696d17d72906
Mateusz Wszola <wszola.m@gmail.com>
2023-04-05 22:00:56 +0200

    reconfigured project with vite

--- src/app/api/hello/route.ts
+++
@@ -1,6 +0,0 @@
-export async function GET(request: Request) {
-  // Return new Response with a JSON body
-  return new Response(JSON.stringify({ message: 'Hello World!' }), {
-    headers: { 'content-type': 'application/json' },
-  });
-}
--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1 className="text-3xl font-bold underline text-orange-500">Reactively</h1>
-      <p>Platform for the Fitness Community</p>
-    </main>
-  )
-}
