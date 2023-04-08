c29548baa600a076b0008c7267e33b98903b233f
Mateusz Wszola <wszola.m@gmail.com>
2023-04-08 18:55:05 +0200

    feat: setup ui library

--- app/root.tsx
+++ app/root.tsx
@@ -1,27 +1,26 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import {Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration} from '@remix-run/react';
+import {MantineProvider, createEmotionCache} from '@mantine/core';
+import {StylesPlaceholder} from '@mantine/remix';
+import {theme} from './theme';
+
+createEmotionCache({key: 'styles'});
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+    return (
+        <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+            <html lang="en">
+            <head>
+                <StylesPlaceholder/>
+                <Meta/>
+                <Links/>
+            </head>
+            <body>
+            <Outlet/>
+            <ScrollRestoration/>
+            <Scripts/>
+            <LiveReload/>
+            </body>
+            </html>
+        </MantineProvider>
+    );
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,13 +1,25 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import type {V2_MetaFunction} from "@remix-run/node";
+import {createStyles, Button} from "@mantine/core";
 
 export const meta: V2_MetaFunction = () => {
-  return [{ title: "Reactively" }];
+    return [{title: "Reactively"}];
 };
 
+const useStyles = createStyles(theme => ({
+    wrapper: {
+        display: 'flex',
+        height: '100px',
+        backgroundColor: theme.colors.blue[5],
+        color: theme.white,
+    }
+}))
+
 export default function Index() {
-  return (
-    <main>
-      <h1>Reactively ✨</h1>
-    </main>
-  );
+    const { classes } = useStyles();
+    return (
+        <main className={classes.wrapper}>
+            <h1>Reactively ✨</h1>
+            <Button c="dimmed">Click me</Button>
+        </main>
+    );
 }
