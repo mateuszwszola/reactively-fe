2b3831ac004c5aa3fe8da56d7f797fc481254c05
Mateusz Wszoła <wszola.m@gmail.com>
2023-04-08 22:45:23 +0200

    Setup UI library and prettier (#2)
    * feat: setup ui library
    
    * feat: setup prettier and reformat code

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,13 +1,25 @@
 import type { V2_MetaFunction } from "@remix-run/node";
+import { createStyles, Button } from "@mantine/core";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "Reactively" }];
 };
 
+const useStyles = createStyles((theme) => ({
+  wrapper: {
+    display: "flex",
+    height: "100px",
+    backgroundColor: theme.colors.blue[5],
+    color: theme.white,
+  },
+}));
+
 export default function Index() {
+  const { classes } = useStyles();
   return (
-    <main>
+    <main className={classes.wrapper}>
       <h1>Reactively ✨</h1>
+      <Button c="dimmed">Click me</Button>
     </main>
   );
 }
