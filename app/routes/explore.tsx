9d800a7588a27d6942142a8737590ad160c8ed16
Mateusz Wszoła <wszola.m@gmail.com>
2023-04-09 17:24:52 +0200

    feat: created login and signup pages ui (#4)

--- app/routes/explore.tsx
+++ app/routes/explore.tsx
@@ -1,3 +1,13 @@
+import type { V2_MetaFunction } from "@remix-run/node";
+
+export const meta: V2_MetaFunction = () => {
+  return [
+    {
+      title: "Explore",
+    },
+  ];
+};
+
 export default function ExplorePage() {
   return (
     <main>
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,7 +1,71 @@
+import {
+  Anchor,
+  Button,
+  Checkbox,
+  Paper,
+  PasswordInput,
+  Text,
+  TextInput,
+  Title,
+} from "@mantine/core";
+import { Link } from "@remix-run/react";
+import type { V2_MetaFunction } from "@remix-run/node";
+import { useStyles } from "~/components/Auth";
+import { IconArrowLeft } from "@tabler/icons-react";
+
+export const meta: V2_MetaFunction = () => {
+  return [
+    {
+      title: "Log In | Reactively",
+    },
+  ];
+};
+
 export default function LoginPage() {
+  const { classes } = useStyles();
+
   return (
-    <main>
-      <h1>Login</h1>
-    </main>
+    <div className={classes.wrapper}>
+      <Paper
+        component="form"
+        method="post"
+        className={classes.form}
+        radius={0}
+        p={30}
+      >
+        <Anchor component={Link} to="/" className={classes.backLink}>
+          <IconArrowLeft />
+          <Text>Homepage</Text>
+        </Anchor>
+        <Title order={2} className={classes.title} ta="center" mt="xl" mb={50}>
+          Welcome back to Reactively!
+        </Title>
+
+        <TextInput
+          name="email"
+          label="Email address"
+          placeholder="hello@gmail.com"
+          size="md"
+        />
+        <PasswordInput
+          name="password"
+          label="Password"
+          placeholder="Your password"
+          mt="md"
+          size="md"
+        />
+        <Checkbox name="loggedIn" label="Keep me logged in" mt="xl" size="md" />
+        <Button type="submit" fullWidth mt="xl" size="md">
+          Login
+        </Button>
+
+        <Text ta="center" mt="md">
+          Don&apos;t have an account?{" "}
+          <Anchor component={Link} to="/signup" weight={600}>
+            Register
+          </Anchor>
+        </Text>
+      </Paper>
+    </div>
   );
 }
