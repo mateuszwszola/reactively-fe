ddc2cbc4e4076fde51dac064e0b8fe3707ca4f3f
Mateusz Wszola <wszola.m@gmail.com>
2023-04-09 17:23:40 +0200

    feat: created login and signup pages ui

---
+++ app/components/Auth.tsx
@@ -0,0 +1,34 @@
+import { createStyles, rem } from "@mantine/core";
+
+export const useStyles = createStyles((theme) => ({
+  wrapper: {
+    minHeight: "100vh",
+    backgroundSize: "cover",
+    backgroundImage:
+      "url(https://images.unsplash.com/photo-1618285818995-4701c3d19269?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80)",
+    backgroundPosition: "center",
+  },
+
+  form: {
+    borderRight: `${rem(1)} solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.colors.gray[3]
+    }`,
+    minHeight: "100vh",
+    maxWidth: rem(450),
+    paddingTop: rem(80),
+
+    [theme.fn.smallerThan("sm")]: {
+      maxWidth: "100%",
+    },
+  },
+
+  title: {
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+  },
+
+  backLink: {
+    display: "flex",
+    alignItems: "center",
+    gap: 4,
+  },
+}));
--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -1,7 +1,78 @@
+import {
+  Anchor,
+  Button,
+  Paper,
+  PasswordInput,
+  Text,
+  TextInput,
+  Title,
+} from "@mantine/core";
+import { Link } from "@remix-run/react";
+import type { V2_MetaFunction } from "@remix-run/node";
+import { useStyles } from "~/components/Auth";
+import { IconArrowLeft } from "@tabler/icons-react";
+
+export const meta: V2_MetaFunction = () => {
+  return [
+    {
+      title: "Sign Up | Reactively",
+    },
+  ];
+};
+
 export default function SignupPage() {
+  const { classes } = useStyles();
+
   return (
-    <main>
-      <h1>Sign up</h1>
-    </main>
+    <div className={classes.wrapper}>
+      <Paper
+        component="form"
+        method="post"
+        className={classes.form}
+        radius={0}
+        p={30}
+      >
+        <Anchor component={Link} to="/" className={classes.backLink}>
+          <IconArrowLeft />
+          <Text>Homepage</Text>
+        </Anchor>
+        <Title order={2} className={classes.title} ta="center" mt="xl" mb={50}>
+          Sign up to get started with Reactively!
+        </Title>
+
+        <TextInput name="login" label="Login" placeholder="Johnny" size="md" />
+        <TextInput
+          name="email"
+          label="Email address"
+          placeholder="johnny@gmail.com"
+          mt="md"
+          size="md"
+        />
+        <PasswordInput
+          name="password"
+          label="Password"
+          placeholder="Your password"
+          mt="md"
+          size="md"
+        />
+        <PasswordInput
+          name="passwordConfirmation"
+          label="Confirm password"
+          placeholder="Confirm password"
+          mt="md"
+          size="md"
+        />
+        <Button type="submit" fullWidth mt="xl" size="md">
+          Sign Up
+        </Button>
+
+        <Text ta="center" mt="md">
+          Already have an account?{" "}
+          <Anchor component={Link} to="/login" weight={600}>
+            Login
+          </Anchor>
+        </Text>
+      </Paper>
+    </div>
   );
 }
