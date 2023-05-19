1317ef7ae6326ea3a584a9dd4dedbb24d62d53e6
Mateusz Wszoła <wszola.m@gmail.com>
2023-05-19 23:14:30 +0200

    feat: add and show comments (#9)

--- app/routes/posts.$postId/route.tsx
+++ app/routes/posts.$postId/route.tsx
@@ -1,16 +1,25 @@
-import { Anchor, Avatar, Box, Button, Flex, Text, Title } from "@mantine/core";
-import { Link, useLoaderData } from "@remix-run/react";
+import {
+  Anchor,
+  Avatar,
+  Box,
+  Button,
+  Text,
+  Textarea,
+  Title,
+  Flex,
+} from "@mantine/core";
 import type { ActionArgs, LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
+import { Form, Link, useLoaderData, useNavigation } from "@remix-run/react";
+import { IconArrowLeft } from "@tabler/icons-react";
+import { fetchFromApi } from "~/client";
+import { requireUserSession } from "~/http";
+import type { Post } from "~/routes/posts._index/route";
 import { getSession } from "~/sessions";
 import type { ResponseBody } from "~/types/api";
 import { API_URL } from "~/types/api";
-import { requireUserSession } from "~/http";
-import { fetchFromApi } from "~/client";
-import type { Post } from "~/routes/posts._index/route";
-import { postSchema } from "~/schemas";
+import { Comment } from "~/components/Comment";
 import React from "react";
-import { IconArrowLeft } from "@tabler/icons-react";
 
 export async function action({ request, params }: ActionArgs) {
   const session = await getSession(request.headers.get("Cookie"));
@@ -50,6 +59,30 @@ export async function action({ request, params }: ActionArgs) {
     }
 
     return data;
+  } else if (intent === "comment") {
+    const response = await fetch(`${API_URL}/comment`, {
+      method: "POST",
+      body: JSON.stringify({
+        id_post: Number(postId),
+        content: formData.get("comment"),
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    const data = await response.json();
+
+    console.log("response", response);
+
+    if (!response.ok) {
+      throw new Response(data, {
+        status: response.status || 400,
+      });
+    }
+
+    return data;
   }
 
   return null;
@@ -78,15 +111,23 @@ export async function loader({ request, params }: LoaderArgs) {
 
   const post = responseData.data[0];
 
-  const parsedPost = postSchema.parse(post);
-
   return json({
-    post: parsedPost,
+    post,
   });
 }
 
 export default function PostPage() {
   const { post } = useLoaderData<typeof loader>();
+  const submission = useNavigation();
+  const commentFormRef = React.useRef<HTMLFormElement>(null);
+
+  React.useEffect(() => {
+    if (submission.formData) {
+      commentFormRef.current?.reset();
+    }
+  }, [submission.formData]);
+
+  const isAddingComment = Boolean(submission.formData);
 
   return (
     <div>
@@ -112,6 +153,43 @@ export default function PostPage() {
         <Title>{post.title}</Title>
 
         <Box mt={32}>{post.content}</Box>
+      </Box>
+
+      <Box mt={120} mx="auto" maw={720}>
+        <Title mb={32} order={2}>
+          Comments
+        </Title>
+
+        <Flex direction="column" gap={32}>
+          {/* TODO: Improve types */}
+          {post.comments.map((comment) => (
+            <Comment
+              key={comment.id_comment}
+              body={comment.content}
+              author={{ name: comment.user.username }}
+              postedAt={comment.createdAt}
+            />
+          ))}
+        </Flex>
+
+        <Box mt={32}>
+          <Form method="post" ref={commentFormRef}>
+            <Textarea
+              name="comment"
+              placeholder="Your comment"
+              label="Your comment"
+            />
+            <Button
+              loading={isAddingComment}
+              mt={16}
+              type="submit"
+              name="intent"
+              value="comment"
+            >
+              Add comment
+            </Button>
+          </Form>
+        </Box>
       </Box>
     </div>
   );
